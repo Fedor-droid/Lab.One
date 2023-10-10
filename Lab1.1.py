b5449257f818221db3a144c190fcf6d313f3ed51
Fedor-droid <Stinger_on06@mail.ru>
2023-10-11 02:23:37 +0500

    Global Update
    В Lab1.1 добавлена возможность вывода чисел с округлением до сотых. Корректировка в Lab1.2. Сделан Readme-файл

--- Lab1.1.py
+++ Lab1.1.py
@@ -1,9 +1,12 @@
-a = int(input('Введите первое число '))
-b = int(input('Введите второе число '))
-print("Сумма чисел", a+b)
-print("Разность чисел", a-b)
-print("Произведение чисел", a*b)
-print("Среднее арифметическое чисел", (a+b)/2 )
+import math
+a = float(input('Введите первое число '))
+b = float(input('Введите второе число '))
+
 max_abs = max(a,b , key=abs)
 min_abs = min(a,b , key=abs)
-print("Разность максимального и минимального по модулю",abs(max_abs) - abs(min_abs))
+
+print("Сумма чисел", round(a+b,2))
+print("Разность чисел", round(a-b,2))
+print("Произведение чисел", round(a*b,2))
+print("Среднее арифметическое чисел", round((a+b)/2 ,2))
+print("Разность максимального и минимального по модулю", round(abs(max_abs) - abs(min_abs),2))
